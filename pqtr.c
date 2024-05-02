2e9c456e928abf782b7816651ade3be80a32b6c9
Brandon Luo <brandon.luo@ll.mit.edu>
2024-05-01 23:13:25 -0400

    Add sign and verify support from command line

--- pqtr.c
+++ pqtr.c
@@ -1,26 +1,42 @@
 #include "pqtr.h"
 
 int
-pqtr_sign(picnic_privatekey_t* sk, const uint8_t* message, size_t message_len,
-                uint8_t* signature, size_t* signature_len)
+pqtr_sign(picnic_privatekey_t *sks, picnic_publickey_t *pks, const uint8_t *message, size_t message_len,
+                uint8_t *signature, size_t *signature_len)
 {
 	return EXIT_SUCCESS;
 }
 
 int
-pqtr_verify(picnic_publickey_t* pk, const uint8_t* message, size_t message_len,
-                  const uint8_t* signature, size_t signature_len)
+pqtr_verify(picnic_publickey_t *pks, const uint8_t *message, size_t message_len,
+                  const uint8_t *signature, size_t signature_len)
 {
 	return EXIT_SUCCESS;
 }
 
 int
-write_key(char *fmt, char *prefix, uint8_t *buf, size_t buflen)
+read_file(char *fmt, char *prefix, int index, uint8_t *buf, size_t buflen)
 {
 	FILE *f;
 	char *pathname;
 
-	asprintf(&pathname, fmt, prefix);
+	asprintf(&pathname, fmt, prefix, index);
+
+	f = fopen(pathname, "w");
+
+	fread(buf, sizeof(buf[0]), buflen, f);
+
+	fclose(f);
+	free(pathname);
+}
+
+int
+write_file(char *fmt, char *prefix, int index, uint8_t *buf, size_t buflen)
+{
+	FILE *f;
+	char *pathname;
+
+	asprintf(&pathname, fmt, prefix, index);
 
 	f = fopen(pathname, "w");
 
@@ -33,51 +49,100 @@ write_key(char *fmt, char *prefix, uint8_t *buf, size_t buflen)
 int
 main(int argc, char *argv[])
 {
-	uint8_t *buf;
-	int bytesRequired, opt;
+	uint8_t *buf, *sigBytes;
+	char *prefix, *msg;
+	int alg, bytesRequired, opt, ret, ringSize, threshold;
+	size_t buflen, sigBytesLen;
+	signature_t *sig;
 
+	ret = 0;
+	ringSize = threshold = 1;
     paramset_t paramset;
 	picnic_params_t parameters = Picnic_L5_UR;
-	picnic_publickey_t pk;
-	picnic_privatekey_t sk;
+	picnic_publickey_t *pks;
+	picnic_privatekey_t *sks;
 
-	while ((opt = getopt(argc, argv, "k:sv")) != -1) {
+	while ((opt = getopt(argc, argv, "km:n:p:st:v")) != -1) {
 		switch (opt) {
 		case 'k':
-			get_param_set(parameters, &paramset);
-			size_t buflen = 1 + 3 * paramset.stateSizeBytes;
-
-			buf = malloc(buflen);
-
-			picnic_keygen(parameters, &pk, &sk);
-
-			bytesRequired = picnic_write_public_key(&pk, buf, buflen);
-			write_key("%s_pk", optarg, buf, bytesRequired);
-
-			bytesRequired = picnic_write_private_key(&sk, buf, buflen);
-			write_key("%s_sk", optarg, buf, bytesRequired);
-
-			free(buf);
-			return EXIT_SUCCESS;
+			alg = KEY_GEN;
+			break;
+		case 'n':
+			ringSize = strtol(optarg, NULL, 10);
+			break;
+		case 'm':
+			msg = optarg;
+			break;
+		case 'p':
+			prefix = optarg;
+			break;
 		case 's':
-			/*
-			read(fd, buf, bufLen);
-			picnic_read_private_key(sk, buf, buflen);
-			picnic_read_public_key(pk, buf, buflen);
-			pqtr_sign();
-			*/
+			alg = SIGN;
+			break;
+		case 't':
+			threshold = strtol(optarg, NULL, 10);
 			break;
 		case 'v':
-			/*
-			read(fd, buf, bufLen);
-			picnic_read_public_key(pk, buf, buflen);
-			pqtr_verify();
-			*/
+			alg = VERIFY;
+			break;
 		default:
 			fprintf(stderr, "usage: %s\n", argv[0]);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	return 0;
+	if (prefix == NULL) {
+		fprintf(stderr, "no prefix\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	get_param_set(parameters, &paramset);
+	buflen = 1 + 3 * paramset.stateSizeBytes;
+	buf = malloc(buflen);
+
+	pks = calloc(ringSize, sizeof(*pks));
+	sks = calloc(threshold, sizeof(*sks));
+
+	switch (alg) {
+	case KEY_GEN:
+			for (int i = 0; i < ringSize; ++i) {
+				picnic_keygen(parameters, &pks[i], &sks[i]);
+
+				bytesRequired = picnic_write_public_key(&pks[i], buf, buflen);
+				write_file("%s_pk_%d", prefix, i, buf, bytesRequired);
+
+				bytesRequired = picnic_write_private_key(&sks[i], buf, buflen);
+				write_file("%s_sk_%d", prefix, i, buf, bytesRequired);
+			}
+			break;
+	case SIGN:
+			for (int i = 0; i < threshold; ++i) {
+				read_file("%s_sk_%d", prefix, i, buf, buflen);
+				picnic_read_private_key(&sks[i], buf, buflen);
+			}
+
+			for (int i = 0; i < ringSize; ++i) {
+				read_file("%s_pk_%d", prefix, i, buf, buflen);
+				picnic_read_public_key(&pks[i], buf, buflen);
+			}
+
+			pqtr_sign(sks, pks, msg, strlen(msg), sigBytes, &sigBytesLen);
+			write_file("%s_%d", "sign", 0, sigBytes, sigBytesLen);
+			break;
+	case VERIFY:
+			for (int i = 0; i < ringSize; ++i) {
+				read_file("%s_pk_%d", prefix, i, buf, buflen);
+				picnic_read_public_key(&pks[i], buf, buflen);
+			}
+
+			ret = pqtr_verify(pks, msg, strlen(msg), sigBytes, sigBytesLen);
+			break;
+	default:
+			fprintf(stderr, "bad alg\n");
+			break;
+	}
+
+	free(buf);
+	return ret;
 }
--- pqtr.h
+++ pqtr.h
@@ -2,9 +2,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "picnic.h"
 #include "picnic_impl.h"
+
+enum Algs { KEY_GEN, SIGN, VERIFY };
 
 #define NUM_COMMITS 128
